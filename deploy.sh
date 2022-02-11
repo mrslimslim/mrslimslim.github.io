a3ebcf384d3ce41e21179f4cee232beb641810fe
mrslimslim <2451319596@qq.com>
2022-02-11 17:00:19 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -4,21 +4,17 @@
 set -e
 
 # 生成静态文件
-# npm run docs:build
-
+npm run docs:build
+rm -rf dist/*
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+cp -r docs/.vuepress/dist/ dist/
 
-rm -rf ./*
+rm -rf docs/.vuepress/dist
+
+
+
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-git init
-git add -A
-git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:mrslimslim/mrslimslim.github.io.git master
-
-
-cd -
